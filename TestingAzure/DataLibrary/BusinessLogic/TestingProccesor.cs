0d6823d7dd76679b1f491bec0e83a8c15d463ee4
Nhat Nguyen <514Nhatnguyen@gmail.com>
2020-10-02 22:04:07 -0400

    Testing

---
+++ TestingAzure/DataLibrary/BusinessLogic/TestingProccesor.cs
@@ -0,0 +1,23 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+   public class TestingProccesor
+    {
+        public static List<TestModel> LoadTest()
+        {
+            string sql = @"select Id, Name, Date
+                            from dbo.TestingTable;";
+
+            return SqlDataAccess.LoadData<TestModel>(sql);
+        }
+    }
+}
