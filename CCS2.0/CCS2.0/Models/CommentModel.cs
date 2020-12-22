8c87cbe0eb104615704fb6890dd1aea98a3720b3
Christian Conder <cconder1998@gmail.com>
2020-12-22 09:30:29 -0500

    Comment Models - No Database yet

---
+++ CCS2.0/CCS2.0/Models/CommentModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCS2._0.Models
+{
+    public class CommentModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Comment.")]
+        [StringLength(256, ErrorMessage = "The Comment cannot exceed 256 characters.")]
+        public string Comment { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name.")]
+        [StringLength(50, ErrorMessage = "The Name cannot exceed 50 characters.")]
+        public string Name { get; set; }
+
+        public Boolean Flag { get; set; }
+
+        [ForeignKey]
+        public int ImageId { get; set; }
+    }
+}
---
+++ CCS2.0/DataLibrary/Models/CommentModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models
+{
+    public class CommentModel
+    {
+        public int Id { get; set; }
+
+        public string Comment { get; set; }
+
+        public string Name { get; set; }
+
+        public Boolean Flag { get; set; }
+
+        public int ImageId { get; set; }
+    }
+}
